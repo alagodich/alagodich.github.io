67a7ef5fae2495cc466bdfa0967835e359c3b966
Anatoly Lagodich <alagodich@northernlight.com>
2015-02-06 18:36:37 -0400

    Polymer one page app init

---
+++ js/content-controller.js
@@ -0,0 +1,91 @@
+(function() {
+"use strict";
+
+    var DEFAULT_ROUTE = 'home';
+    var template = document.querySelector('template[is="auto-binding"]');
+    var contentController, pages, scaffold;
+    var cache = {};
+
+    template.pages = [
+        {name: 'Home', hash: 'home', url: '/home.html'},
+        {name: 'Dart sample1', hash: 'dart-sample', url: '/dart-sample.html'},
+        {name: 'Polymer sample', hash: 'polymer-sample', url: '/polymer-sample.html'}
+    ];
+
+
+    template.addEventListener('template-bound', function(e) {
+        scaffold = document.querySelector('#scaffold');
+        contentController = document.querySelector('#contentController');
+        pages = document.querySelector('#pages');
+        var keys = document.querySelector('#keys');
+
+        // Use URL has for initial route. Otherwise, use the first page.
+        this.route = this.route || DEFAULT_ROUTE;
+    });
+
+
+    // Prevent link navigation
+    template.ajaxLoad = function(e, detail, sender) {
+        e.preventDefault();
+    };
+
+
+    template.keyHandler = function(e, detail, sender) {
+
+        switch (detail.key) {
+            case 'left':
+            case 'up':
+                pages.selectPrevious();
+                break;
+            case 'right':
+            case 'down':
+                pages.selectNext();
+                break;
+            case 'space':
+                detail.shift ? pages.selectPrevious() : pages.selectNext();
+                break;
+        }
+    };
+
+    template.menuItemSelected = function(e, detail, sender) {
+        if (detail.isSelected) {
+
+            // Need to wait so <core-ajax> has it's URL set
+            this.async(function() {
+                if (!cache[contentController.url]) {
+                    contentController.go();
+                }
+                scaffold.closeDrawer();
+            });
+        }
+    };
+
+    template.cyclePages = function(e, detail, sender) {
+        // Clicks should navigate and not cycle pages
+        if (e.path[0].localName == 'a') {
+            return;
+        }
+        e.shiftKey ? sender.selectPrevious(true) : sender.selectNext(true);
+    };
+
+    template.onResponse = function(e, detail, sender) {
+        console.log('test');
+        var article = detail.response.querySelector('#article-content');
+        console.log(detail.response);
+        //article.querySelector('.byline').remove();
+
+        // Fix up image paths to not be local
+        [].forEach.call(article.querySelectorAll('img'), function(img) {
+            img.setAttribute('src', img.src);
+        });
+
+        var html = article.innerHTMl;
+
+        // Primitive caching by URL
+        cache[contentController.url] = html;
+
+        this.injectBoundHTML(html, pages.selectedItem.firstElementChild);
+    };
+
+})();
+
