0b44f5389e123c977f528987e163c9c935fdce0d
Anatoly Lagodich <alagodich@northernlight.com>
2021-01-12 03:25:51 -0500

    Handle more iReal Pro song urls

---
+++ public/metronomeworker.js
@@ -0,0 +1,1 @@
+(()=>{var t=null,a=100;self.onmessage=function(e){"start"===e.data?t=setInterval((function(){postMessage("tick")}),a):e.data.interval?(a=e.data.interval,t&&(clearInterval(t),t=setInterval((function(){postMessage("tick")}),a))):"stop"===e.data&&(clearInterval(t),t=null)}})();
